5b8e4d941661d8307ccd47d848891eb24efaa531
Theo Ford <theo.e.ford@gmail.com>
2023-08-04 15:51:25 +0100

    load tests 8

--- src/pages/about17.js
+++ src/pages/about17.js
@@ -364,6 +364,25 @@ const ListCol4 = styled.div`
 const About = ({ data }) => {
   let isPageWide = useMediaQuery("(min-width: 667px)");
   const LogoConRef = useRef(null);
+  const [pageLoad, setPageLoad] = useState(null);
+
+  useEffect(() => {
+    // callback function to call when event triggers
+    const onPageLoad = () => {
+      console.log("page loaded");
+      setPageLoad(true);
+      // do something else
+    };
+
+    // Check if the page has already loaded
+    if (document.readyState === "complete") {
+      onPageLoad();
+    } else {
+      window.addEventListener("load", onPageLoad, false);
+      // Remove the event listener when component unmounts
+      return () => window.removeEventListener("load", onPageLoad);
+    }
+  }, []);
 
   const LogoNav = scrollPosition => {
     if (isPageWide) {
@@ -373,7 +392,7 @@ const About = ({ data }) => {
           <LogoGridCon>
             <Grid2B>
               <Col1>
-                <LogoCon ref={LogoConRef}>
+                <LogoCon ref={LogoConRef} style={{ opacity: pageLoad ? 1 : 0 }}>
                   <Icon />
                 </LogoCon>
               </Col1>
