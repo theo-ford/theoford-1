147e32b69bcb5575947126ea5b231c39adfd2dc5
Theo Ford <theoford@Theos-MBP.cust.communityfibre.co.uk>
2023-10-18 18:54:45 +0100

    adding swiping to mobile carousels

--- src/components/tf/index/one-up-carousel/one-up-carousel-swiper.js
+++ src/components/tf/index/one-up-carousel/one-up-carousel-swiper.js
@@ -70,6 +70,32 @@ export const OneUpProjectCarouselSwiper = ({
 
   projectLength,
 }) => {
+  const [touchStart, setTouchStart] = useState(null);
+  const [touchEnd, setTouchEnd] = useState(null);
+  // the required distance between touchStart and touchEnd to be detected as a swipe
+
+  const minSwipeDistance = 50;
+
+  const onTouchStart = e => {
+    setTouchEnd(null); // otherwise the swipe is fired even with usual touch events
+    setTouchStart(e.targetTouches[0].clientX);
+  };
+  const onTouchMove = e => setTouchEnd(e.targetTouches[0].clientX);
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return;
+    const distance = touchStart - touchEnd;
+    const isLeftSwipe = distance > minSwipeDistance;
+    const isRightSwipe = distance < -minSwipeDistance;
+    if (isLeftSwipe || isRightSwipe)
+      if (isLeftSwipe) {
+        // console.log("swipe", isLeftSwipe ? "left" : "right");
+        // add your conditional logic here
+        swiperRef.current.swiper.slideNext();
+      } else if (isRightSwipe) {
+        swiperRef.current.swiper.slidePrev();
+      }
+  };
+
   const swiperRef = useRef(null);
   function forwardFunc() {
     swiperRef.current.swiper.slideNext();
@@ -84,7 +110,11 @@ export const OneUpProjectCarouselSwiper = ({
           <p onClick={forwardFunc}>Next</p>
         </NextButtonConP>
       </Grid16>
-      <SwiperCarouselCon>
+      <SwiperCarouselCon
+        onTouchStart={onTouchStart}
+        onTouchMove={onTouchMove}
+        onTouchEnd={onTouchEnd}
+      >
         <ButtonCon>
           <PrevButton onClick={prevFunc}></PrevButton>
           <NextButton onClick={forwardFunc}></NextButton>
