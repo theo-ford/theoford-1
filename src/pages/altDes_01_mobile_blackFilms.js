f0c297921dfcd8bac18d20ce721eb589eec7345b
Theo Ford <theoford@Theos-MBP.communityfibre.co.uk>
2024-10-04 17:29:07 +0100

    20241004 17:28 altDes_01_mobile_blackFilms

--- src/pages/altDes_01_mobile_blackFilms.js
+++ src/pages/altDes_01_mobile_blackFilms.js
@@ -437,7 +437,7 @@ const Index = ({ data }) => {
 export default withPrismicPreview(Index);
 
 export const query = graphql`
-  query IndexQuery70 {
+  query IndexQuery74 {
     prismicFeaturedProjects {
       data {
         project_relationship_group {
